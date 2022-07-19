a65c503cb617c13c2bc004fd9cfaf1585a982073
linguopeng <linguopeng@hyperchain.cn>
2022-07-19 21:50:32 +0800

    test: adapt 1.4.0

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,26 +3,27 @@ require("@nomiclabs/hardhat-web3");
 // var initHre = require("./hardhat/hardhat").initHre;
 
 const INFURA_PROJECT_ID = "719d739434254b88ac95d53e2b6ac997";
-// eth_address: 0x0c1efcca2bcb65a532274f3ef24c044ef4ab6d73
-const PRIVATE_KEY = "dd50cac37ec6dd12539a968c1a2cbedda75bd8724f7bcad486548eaabb87fc8b";
+// eth_address: 0x4866f8c3d21CDb7CFf6689576cAeA91A475EA98a
+const PRIVATE_KEY = "300d786d3e31e5c928332f4cc62318ac613252938c64ebf1213f5e6a6ccf4bad";
 
-// 0x40711aDc577DE17232DD6F997022F68BE6BE8560
-const PRIVATE_KEY2 = "5f571783b4be167c4b8d80aaabc74e15c5b954f2d815fbdeadae01421501119f"
-
-
-// eth_address: 0x934F1EbCB57ce1D9985A4c4f8811D17B04342067
-const PRIVATE_KEY3 = "31227350280dbcb27d2976c900faf62ce92c2ad0b982541cc837fb9236b3b415"
-
-// eth_address: 0xC4A4281378FE711d2e02480Cb7568952EB249fdF
-const PRIVATE_KEY4 = "9269aa582ad09351383b32e7981f84f6efa5f94a9322813f63894caff74166a0"
-
-// eth_address: 0x6B75801d17BcB7dbA0bDfE1d989e49A722e020e6
-const PRIVATE_KEY5 = "b7900989d6fa12c9048e331eeb7f4cb34f224a5cb951ab88a8a289fddc3bec91"
-
-// eth_address: 0x8974eD1cfA93B1E28E11005E3eA88AF81452113a
-const PRIVATE_KEY6 = "7645889bd8772bad4daeca099a72c90e476865d1f06275dbbc5cad1e3259163a"
+// 0x66bf05FDA3d5fA21dbf72e418a320A24fAC799c6
+const PRIVATE_KEY2 = "bb2f7df4868c09050313e8de72d26360cb52f441b078316ffc07f35bcb7282c6"
 
 
+// eth_address: 0xb3d645264a3cc2A0224A2331E6c1B4ca8b2cadFc
+const PRIVATE_KEY3 = "0090e235be7a79858e108af8fbb04c084a81460674b48411f600a82521a177c4"
+
+// eth_address: 0x9dfBaaF3f328D9a7abe2916cf65B11BAba6D0248
+const PRIVATE_KEY4 = "ed6e3fc8de3b34c692575631612b4d1258becf61edfba5be1313274ec3ff9f79"
+
+//0x75B8EF89dB9376EC7003F6EC79D14C49a9aF10db
+const PRIVATE_KEY5 = "8b9adda837c9f2f26be59f3712226d41734e3f150842faf459cefdec5af489b3"
+
+// eth_address: 0x3402959Ae126abbEf7a9F61772b5003015B19837
+const PRIVATE_KEY6 = "1645b6af3e8c08278351194cdcb8198a21aca22ed097a1c7a83ac8d9eb6d11c4"
+
+// unregister address
+const PRIVATE_KEY7 = "29785e7e193781be1a0d5a75e1ed603739efe91a268e32690313f1606154609b"
 /**
  * @type import('hardhat/config').HardhatUserConfig
  *
@@ -34,22 +35,46 @@ module.exports = {
             loggingEnabled: true,
             allowUnlimitedContractSize: true
         },
+
+        axon_remote: {
+            url: 'http://192.168.10.174:8000',
+            // gasMultiplier: 2,
+            gas:5000000,
+            accounts: {
+                mnemonic: "test test test test test test test test test test test junk",
+                path: "m/44'/60'/0'/0",
+                initialIndex: 0,
+                count: 20,
+                passphrase: "",
+            },
+        },
         bsc_test: {
             url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
             accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`,`0x${PRIVATE_KEY3}`,`0x${PRIVATE_KEY4}`,`0x${PRIVATE_KEY5}`,`0x${PRIVATE_KEY6}`]
         },
 
+        axon_test: {
+            url: 'http://localhost:8000',
+            accounts: {
+                mnemonic: "test test test test test test test test test test test junk",
+                path: "m/44'/60'/0'/0",
+                initialIndex: 2,
+                count: 10,
+                passphrase: "",
+            },
+        },
+//https://godwoken-testnet-v1.ckbapp.dev/
         gw_local_kit_net_v1: {
             url: `http://127.0.0.1:8024`,
-            accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`,`0x${PRIVATE_KEY3}`,`0x${PRIVATE_KEY4}`,`0x${PRIVATE_KEY5}`,`0x${PRIVATE_KEY6}`]
+            accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`,`0x${PRIVATE_KEY3}`,`0x${PRIVATE_KEY4}`,`0x${PRIVATE_KEY5}`,`0x${PRIVATE_KEY6}`,`0x${PRIVATE_KEY7}`]
         },
         gw_testnet_v1: {
-            url: `https://godwoken-testnet-web3-v1-rpc.ckbapp.dev`,
-            accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`,`0x${PRIVATE_KEY3}`,`0x${PRIVATE_KEY4}`,`0x${PRIVATE_KEY5}`,`0x${PRIVATE_KEY6}`]
+            url: `https://godwoken-testnet-v1.ckbapp.dev/`,
+            accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`,`0x${PRIVATE_KEY3}`,`0x${PRIVATE_KEY4}`,`0x${PRIVATE_KEY5}`,`0x${PRIVATE_KEY6}`,`0x${PRIVATE_KEY7}`]
         },
         gw_testnet_v11: {
             url: `https://godwoken-betanet-v1.ckbapp.dev`,
-            accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`,`0x${PRIVATE_KEY3}`,`0x${PRIVATE_KEY4}`,`0x${PRIVATE_KEY5}`,`0x${PRIVATE_KEY6}`]
+            accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`,`0x${PRIVATE_KEY3}`,`0x${PRIVATE_KEY4}`,`0x${PRIVATE_KEY5}`,`0x${PRIVATE_KEY6}`,`0x${PRIVATE_KEY7}`]
         },
         gw_testnet_v0: {
             url: `https://godwoken-testnet-web3-rpc.ckbapp.dev`,
@@ -58,10 +83,17 @@ module.exports = {
         },
         gw_alphanet_v1: {
                 url: `https://godwoken-alphanet-v1.ckbapp.dev`,
-                accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`,`0x${PRIVATE_KEY3}`,`0x${PRIVATE_KEY4}`,`0x${PRIVATE_KEY5}`,`0x${PRIVATE_KEY6}`]
+                accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`,`0x${PRIVATE_KEY3}`,`0x${PRIVATE_KEY4}`,`0x${PRIVATE_KEY5}`,`0x${PRIVATE_KEY6}`,`0x${PRIVATE_KEY7}`]
             },
         rinkeby: {
             url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+            accounts: [`0x${PRIVATE_KEY}`]
+            // gas: 1_000_000_000_000_001, // Infura seems to cap it at 19981536.
+            // gasPrice: 1
+        },
+        //https://ropsten.infura.io/v3/
+        ropsten: {
+            url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
             accounts: [`0x${PRIVATE_KEY}`]
             // gas: 1_000_000_000_000_001, // Infura seems to cap it at 19981536.
             // gasPrice: 1
@@ -73,16 +105,31 @@ module.exports = {
         kovan: {
             url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
             accounts: [`0x${PRIVATE_KEY}`]
-        }
+        },
+        localEth: {
+            url: 'http://127.0.0.1:8545',
+            accounts: {
+                mnemonic: 'test test test test test test test test test test test junk',
+                path: 'm/44\'/60\'/0\'/0',
+                initialIndex: 0,
+                count: 20,
+                passphrase: '',
+            },
+        },
         // hardhat: {
         //   gas: 1000000000000, // Infura seems to cap it at 19981536.
         //   gasPrice: 1
         // }
     },
+
+
     //bsc_test
-    // defaultNetwork: "gw_alphanet_v1", //gw_local_kit_net_v1 gw_testnet_v11
-    // defaultNetwork: "rinkeby",
-    defaultNetwork: "gw_local_kit_net_v1",
+    // defaultNetwork:"gw_alphanet_v1",
+    defaultNetwork:"localEth",
+
+    // defaultNetwork: "ropsten", //gw_local_kit_net_v1 gw_testnet_v11
+    // defaultNetwork: "axon_test",
+    // defaultNetwork: "gw_testnet_v1",
     solidity: {
         compilers: [
             { // for polyjuice contracts
