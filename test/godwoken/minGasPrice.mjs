3717c4068058d9e1cf1213bc54bd44a7f312e222
sunchengzhu <36075573+sunchengzhu@users.noreply.github.com>
2022-12-16 17:06:30 +0800

    补丁包误删了 再传一下 (#33)

--- test/godwoken/minGasPrice.mjs
+++ test/godwoken/minGasPrice.mjs
@@ -6,9 +6,9 @@ describe("minGasPrice", function () {
     it("get gasPrice", async () => {
         let gasPrice = await getGasPrice(ethers.provider);
         let ethGasPrice = ethers.utils.formatEther(parseInt(gasPrice, 16));
-        let latestBlock = await ethers.provider.getBlock("latest")
-        console.log(ethGasPrice, latestBlock.number)
-    }).timeout(5000)
+        let latestBlock = await ethers.provider.getBlockNumber();
+        console.log(ethGasPrice, latestBlock);
+    }).timeout(50000)
 })
 
 async function getGasPrice(provider) {
